9198379675c44365c946aee1cc683e5a11dbf01b
NGUYEN Nhat Minh <naht-minh.nguyen_externe@groupe-sma.fr>
2019-02-22 16:07:37 +0100

    add auth core

--- src/index.js
+++ src/index.js
@@ -1,12 +1,19 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { store } from './_helpers';
+import { App } from './App';
+// setup fake backend
+import { configureFakeBackend } from './_helpers';
 
-ReactDOM.render(<App />, document.getElementById("root"));
+configureFakeBackend();
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
+
 serviceWorker.unregister();
